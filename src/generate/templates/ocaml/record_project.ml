360ec781ba05670c885f77e15c40c7d292ff7747
zach <zachshipko@gmail.com>
2022-07-29 19:36:43 -0700

    Fix ocaml formatting

--- src/generate/templates/ocaml/record_project.ml
+++ src/generate/templates/ocaml/record_project.ml
@@ -1,5 +1,6 @@
-let get_{name} t =
-  let out = allocate_n ~count:1 {s} in
-  let rc = Bindings.{project} t.opaque_ctx.Context.handle out t.opaque_ptr in
-  if rc <> 0 then raise (Error (Code rc));
-  {out}
+  let get_{name} t =
+    let out = allocate_n ~count:1 {s} in
+    let rc = Bindings.{project} t.opaque_ctx.Context.handle out t.opaque_ptr in
+    if rc <> 0 then raise (Error (Code rc));
+    {out}
+
