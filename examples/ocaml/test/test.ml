dc0ea787e64cce2233b5d11726b76412421e078c
zach <zachshipko@gmail.com>
2022-08-10 13:34:41 -0700

    Use of_array in tests

--- examples/ocaml/test/test.ml
+++ examples/ocaml/test/test.ml
@@ -11,25 +11,26 @@ let () =
   Array_i64_1d.free arr;
 
   (* mul2 *)
-  let data2 = Array2.of_array Float64 C_layout [| [| 1.0; 2.0; 3.0 |]; [| 4.0; 5.0; 6.0 |]|] |> genarray_of_array2 in
-  let arr = Array_f64_2d.v ctx data2 in
-  Printf.printf "%f\n" (Genarray.get data2 [| 0; 0 |]);
+  let data2 = [| 1.0; 2.0; 3.0; 4.0; 5.0; 6.0 |] in
+  let arr = Array_f64_2d.of_array ctx [| 2; 3 |] data2 in
+  Printf.printf "%f\n" (data2.(0));
   let out = mul2 ctx arr in
   let out' = Array_f64_2d.get out in
   Printf.printf "%f\n" (Genarray.get out' [| 0; 0 |]);
   assert (Genarray.get out' [| 0; 0 |] = 2.0);
+  assert (Genarray.get out' [| 1; 2 |] = 12.0);
   Array_f64_2d.free out;
   Array_f64_2d.free arr;
 
   (* tup_mul *)
   let number = Number.v ctx 2.5 in
-  let data3 = Array1.of_array Float32 C_layout [| 0.0; 1.0; 2.0; 3.0; 4.0; 5.0; 6.0; 7.0; 8.0; 9.0 |] |> genarray_of_array1 in
-  let arr = Array_f32_1d.v ctx data3 in
+  let data3 = [| 0.0; 1.0; 2.0; 3.0; 4.0; 5.0; 6.0; 7.0; 8.0; 9.0 |] in
+  let arr = Array_f32_1d.of_array ctx [| Array.length data3 |] data3 in
   let tup = Tup.v ctx number arr in
   let out = tup_mul ctx tup in
   let out' = Array_f32_1d.get out |> array1_of_genarray in
   for i = 0 to 9 do
-    assert (out'.{i} = Genarray.get data3 [| i |] *. (Number.get_x (Tup.get_0 tup)))
+    assert (out'.{i} = Array.get data3 i *. (Number.get_x (Tup.get_0 tup)))
   done;
 
   (* count_lines *)
